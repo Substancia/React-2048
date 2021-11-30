f81a3f8536eb33f5a4f095a8ee587664d5a6031e
Sentienta <sentientasins@gmail.com>
2021-11-30 15:35:08 +0530

    added variable colors to cells

--- src/components/Cell/index.jsx
+++ src/components/Cell/index.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import './index.css';
 
 const Cell = ({ value }) => {
+  const order = Math.log2(value) - 1;
+
   return (
-    <div className='Cell'>
+    <div className='Cell'
+      style={{ backgroundColor: value !== null ?
+          `hsl(51, 100%, ${80 - 30 * (order / 15)}%)` :
+          'coral'
+      }}
+    >
       {
         value !== null && value
       }
